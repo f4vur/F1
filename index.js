08c43483f25063f3ec58aa383bea6362584f5b22
Favour I <favourdeborah008@gmail.com>
2025-07-26 17:41:52 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("=========================");
-console.log("             F1          ");
+console.log("             F1            ");
 console.log("-------------------------");
 console.log("      Lewis Hamilton     ");
